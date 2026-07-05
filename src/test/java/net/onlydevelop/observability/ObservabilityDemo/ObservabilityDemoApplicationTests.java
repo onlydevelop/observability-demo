a01e4af0a22c303e48576ad4dd8ecc475abbb28a
Dipanjan Bhowmik <onlydevelop@gmail.com>
2026-07-05 10:12:51 +0530

    Inital commit

---
+++ src/test/java/net/onlydevelop/observability/ObservabilityDemo/ObservabilityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.onlydevelop.observability.ObservabilityDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
